607071025eb41de8ec1d01b1cd777fc4e2f34b22
Joona Heinonen <joona.heinonen@windowslive.com>
2022-04-26 11:00:36 +0300

    Comment added
    Joona teki muudoksen AddQuestion.javaan

--- Vaalikone_v3/src/main/java/app/AddQuestion.java
+++ Vaalikone_v3/src/main/java/app/AddQuestion.java
@@ -22,7 +22,7 @@ public class AddQuestion extends HttpServlet {
 	private Dao dao = null;
 
 	// VAALIKONE_V3 REPO SETTING
-
+	// Joona teki muudoksen
 	@Override
 	public void init() {
 		dao = new Dao("jdbc:mysql://localhost:3306/vaalikone?useSSL=false", "sikli", "kukkuu");
